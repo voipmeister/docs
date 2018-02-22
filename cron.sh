5abdaaf8a10575b56c49251523ecd0813f9d0884
nabeelio <nshahzad@gmail.com>
2018-02-22 23:13:17 +0000

    content updates

--- cron.sh
+++ cron.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cd /var/www/docs.phpvms/
+cd /var/www/docs.phpvms
 #chmod -R 0775 user
 
 # Sync all of the docs with git
-bin/plugin git-sync sync
+sudo -u www-data bin/plugin git-sync sync
 
 # Download the CHANGELOG file and move it
 wget https://raw.githubusercontent.com/nabeelio/phpvms/master/CHANGELOG.md -O /tmp/CHANGELOG.md
-cp /tmp/CHANGELOG.md user/pages/changelog/default.md
+cp /tmp/CHANGELOG.md /var/www/docs.phpvms/user/pages/changelog/default.md
+chown www-data:www-data /var/www/docs.phpvms/user/pages/changelog/default.md
+rm /tmp/CHANGELOG.md
 
-
+sudo -u www-data bin/grav clear-cache
